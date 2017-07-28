f9f962c23511a0893165e2957cfb081368492b29
marco <ti.marco@publicacoesonline.com.br>
2017-07-28 07:40:28 -0300

    com express validator

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 var app = express();
 app.set('view engine', 'ejs');
@@ -8,6 +9,7 @@ app.set('views', './app/views');
 
 //body-parser é um middleware, utiliza atraves do app use
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(expressValidator());
 
 
 //com o consign realiza o autoload dos arquivos
